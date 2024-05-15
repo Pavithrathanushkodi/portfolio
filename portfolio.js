3b9381cfa9a4322c45fcf413421bca552611c898
Pavithrathanushkodi <158037361+Pavithrathanushkodi@users.noreply.github.com>
2024-05-15 19:06:34 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+
+        function callPhoneNumber() {
+            window.location.href = "tel:+91 6380799877"; // Replace with your phone number
+        }
+    
+        function sendEmail() {
+            window.location.href = "mailto:your-email@example.com"; // Replace with your email address
+        }
+
+        function openLinkedIn() {
+            window.open('https://www.linkedin.com/in/your-profile', '_blank'); // Replace with your LinkedIn profile URL
+        }
